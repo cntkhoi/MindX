59697819c5d6133ad9789776eab2f700d132c011
cntkhoi <cntkhoi@gmail.com>
2023-03-29 20:37:54 +0700

    29Mar01

---
+++ Lesson 10/js/app.js
@@ -0,0 +1,47 @@
+let names = [
+    "Chinh Do",
+    "Ngoc Trinh",
+    "Chi Pu",
+    "Dam Vinh Hung"
+];
+
+// for (let i = 0; i< names.length; i++){
+//     let stt = i + 1;
+//     // console.log(stt + " " + names[i]);
+
+//     let name = names[i];
+//     if (name == "Ngoc Trinh" || name == "Chi Pu") {
+//         console.log(stt + ". " + name);
+//     }
+// }
+
+
+let arr = [10,90,50,30,20,70,100,40,80,60];
+let max = arr[0];
+let sum = 0;
+//in ra so chan trong numbers
+for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+}
+    let average = sum / arr.length;
+    console.log('average: ' + average);
+
+
+
+
+
+
+
+    // console.log('vi tri :', arr[i]);
+    // res *= arr[i];
+    // console.log('vi tri :', res);
+
+    // console.log(arr[i]);
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+// }
+
+
+// console.log('title: ', max);
+//tinh tong so phan tu
